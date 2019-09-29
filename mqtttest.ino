76ff84e357d4b8d96d581796ed58810ea4ad0b82
Khang <48411041+Minatozaki0911@users.noreply.github.com>
2019-09-29 12:08:02 +0700

    Create mqtttest.ino

---
+++ mqtttest.ino
@@ -0,0 +1,67 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "wifi nè";
+const char* password =  "pass nè";
+const char* mqttServer = "soldier.cloudmqtt.com";
+const int mqttPort = 12246;
+const char* mqttUser = "cvpsnfab";
+const char* mqttPassword = "1GwFw2V54EBe";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("esp/sensor", "There's nothing here, go away baka");
+  client.subscribe("esp/sensor");
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void loop() {
+  client.loop();
+}
